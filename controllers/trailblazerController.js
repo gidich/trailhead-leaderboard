b13064502abbe7c7ea10b3fee5f2c4bb603df4b0
Patrick Gidich <patrick@simnova.com>
2019-04-27 22:02:02 -0400

    Adding More Certs

--- controllers/trailblazerController.js
+++ controllers/trailblazerController.js
@@ -39,6 +39,255 @@ exports.trailblazer_list = async function(req,res){
             }
         }
     );
+
+    var adminCert = await trailblazerFactory.aggregate(
+        [
+            {
+                $project: {
+                    trailblazerId:1,
+                    full_name:1,
+                    badges: {
+                        $size: {
+                            $filter: {
+                                input: "$badges",
+                                as: "badge",
+                                cond: {
+                                        $in:[
+                                            '$$badge.title',
+                                            [
+                                                ' Organizational Setup (Global UI)',
+                                                ' Salesforce User Basics',
+                                                ' Salesforce Platform Basics',
+                                                ' Company-Wide Org Settings',
+                                                ' User Authentication',
+                                                ' Prepare Your Salesforce Org for Users',
+                                                ' Salesforce User Setup',
+                                                ' User Management',
+                                                ' Customize an Org to Support a New Business Unit',
+                                                ' Salesforce Security and Access',
+                                                ' How to Successfully Navigate Salesfor...',
+                                                ' Data Security',
+                                                ' Control Who Sees What',
+                                                ' Identity Basics',
+                                                ' Activity Management',
+                                                ' Security Specialist',
+                                                ' Standard and Custom Objects',
+                                                ' Data Modeling',
+                                                ' Lightning Experience Customization',
+                                                ' Customize a Salesforce Object',
+                                                ' Sales and Marketing Applications',
+                                                ' Accounts & Contacts for Lightning Experience',
+                                                ' Leads & Opportunities for Lightning Experience',
+                                                ' Products, Quotes, & Contracts',
+                                                ' Salesforce Content Overview',
+                                                ' Marketing Cloud Basics',
+                                                ' Campaign Basics',
+                                                ' Customize a Sales Path for Your Team',
+                                                ' Service and Support Applications',
+                                                ' Service Cloud for Lightning Experience',
+                                                ' Knowledge Search Basics',
+                                                ' Knowledge Basics for Lightning Experience',
+                                                ' Community Cloud Basics',
+                                                ' Build a Community with Knowledge and Chat',
+                                                ' Activity Management and Collaboration',
+                                                ' Activity Management',
+                                                ' Chatter Administration for Lightning Experience',
+                                                ' Data Management',
+                                                ' Data Quality',
+                                                ' Data Management',
+                                                ' Duplicate Management',
+                                                ' Import and Export with Data Management Tools',
+                                                ' Analytics - Reports and Dashboards',
+                                                ' Reports & Dashboards for Salesforce Classic',
+                                                ' Reports & Dashboards for Lightning Experience',
+                                                ' Create Reports and Dashboards for Sales and Marketing Managers',
+                                                ' Filter Report Data',
+                                                ' Lightning Experience Reports & Dashboards Specialist',
+                                                ' Workflow / Process Automation',
+                                                ' Workflow',
+                                                ' Lightning Flow',
+                                                ' Workflow Rule Migration',
+                                                ' Build a Discount Approval Process',
+                                                ' Quick Start: Process Builder',
+                                                ' Build a Mars Communication Uplink',
+                                                ' Build a Battle Station App',
+                                                ' Desktop and Mobile Administration',
+                                                ' Salesforce Mobile App Customization',
+                                                ' Outlook Integration',
+                                                ' AppExchange',
+                                                ' AppExchange Basics',
+                                                ' Business Administration Specialist',
+                                                ' Certified Administrator Practice Test',
+                                                ' Session-Based Permission Sets and Security',
+                                                ' Lightning Experience Rollout Specialist',
+                                            ]
+                                        ]
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $sort: {badges:-1}
+            }
+        ]
+    );
+
+    var advancedAdminCert = await trailblazerFactory.aggregate(
+        [
+            {
+                $project: {
+                    trailblazerId:1,
+                    full_name:1,
+                    badges: {
+                        $size: {
+                            $filter: {
+                                input: "$badges",
+                                as: "badge",
+                                cond: {
+                                        $in:[
+                                            '$$badge.title',
+                                            [
+                                                ' Security and Access',
+                                                ' Formulas & Validations',
+                                                ' Picklist Administration',
+                                                ' Session-Based Permission Sets and Security',
+                                                ' Transaction Security',
+                                                ' Configure Enterprise Territory Manage...',
+                                                ' Extending Custom Objects and Applicat...',
+                                                ' Considerations for Relationships',
+                                                ' Manage Custom Objects',
+                                                ' Lightning Experience Rollout Specialist',
+                                                ' Auditing and Monitoring',
+                                                ' Manage Events',
+                                                ' Event Monitoring',
+                                                ' Event Monitoring Analytics App',
+                                                ' Sales Cloud Applications',
+                                                ' Products, Quotes, & Contracts',
+                                                ' Collaborative Forecasts Setup',
+                                                ' Collaborative Forecasts Configuration',
+                                                ' Territory Management Basics',
+                                                ' Territory Management Best Practices',
+                                                ' Path & Workspaces',
+                                                ' Service Cloud Applications',
+                                                ' Entitlement Management',
+                                                ' Lightning Knowledge Setup and Customization',
+                                                ' Set Up Case Escalation and Entitlements',
+                                                ' Set Up Salesforce Knowledge',
+                                                ' Omni-Channel for Lightning Experience',
+                                                ' Case Feed',
+                                                ' Identity for Customers',
+                                                ' Data Management',
+                                                ' Duplicate Management',
+                                                ' Import and Export with Data Management Tools',
+                                                ' Improve Data Quality for Your Sales and Support Teams',
+                                                ' Improve Data Quality for a Recruiting App',
+                                                ' Content Management',
+                                                ' Set Up Salesforce CRM Content',
+                                                ' Knowledge Article Visibility and Data...',
+                                                ' Change Management',
+                                                ' Application Lifecycle and Development Models',
+                                                ' Change Set Development Model',
+                                                ' Analytics - Reports and Dashboards',
+                                                ' Evaluate Report Data with Summary Formulas',
+                                                ' Embed Dashboards and Report Charts on Lightning Pages',
+                                                ' Process Automation',
+                                                ' Approvals',
+                                                ' Advanced Formulas',
+                                                ' Leads & Opportunities for Lightning Experience',
+                                            ]
+                                        ]
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $sort: {badges:-1}
+            }
+        ]
+    );
+
+    var platformAppBuilderCert = await trailblazerFactory.aggregate(
+        [
+            {
+                $project: {
+                    trailblazerId:1,
+                    full_name:1,
+                    badges: {
+                        $size: {
+                            $filter: {
+                                input: "$badges",
+                                as: "badge",
+                                cond: {
+                                        $in:[
+                                            '$$badge.title',
+                                            [
+                                                ' App Builder Exam Overview',
+                                                ' Salesforce Fundamentals',
+                                                ' CRM for Lightning Experience',
+                                                ' AppExchange Basics',
+                                                ' Platform Development Basics',
+                                                ' Data Modeling and Management',
+                                                ' Accounts & Contacts for Lightning Experience',
+                                                ' Leads & Opportunities for Lightning Experience',
+                                                ' Data Modeling',
+                                                ' Data Management',
+                                                ' Salesforce Connect',
+                                                ' Formulas Cheat Sheet',
+                                                ' Security',
+                                                ' Data Security',
+                                                ' Identity Basics',
+                                                ' Security Basics',
+                                                ' Security for Admins cheat sheet',
+                                                ' Security Specialist',
+                                                ' Business Logic and Process Automation',
+                                                ' User Management',
+                                                ' Formulas & Validations',
+                                                ' Flow Basics',
+                                                ' Lightning Flow',
+                                                ' Process Automation cheat sheet',
+                                                ' Which Automation Tool Do I Use?',
+                                                ' Quick Start: Process Builder',
+                                                ' Process Automation Specialist',
+                                                ' Social',
+                                                ' Chatter for Lightning Experience',
+                                                ' User Interface',
+                                                ' Aura Components Basics',
+                                                ' Lightning Design System',
+                                                ' Lightning App Builder',
+                                                ' Quick Start: Aura Components',
+                                                ' Lightning Experience Specialist',
+                                                ' Reporting',
+                                                ' Reports & Dashboards for Lightning Experience',
+                                                ' Quick Start: Reports & Dashboards',
+                                                ' Lightning Experience Reports & Dashboards Specialist',
+                                                ' Mobile',
+                                                ' Salesforce Mobile App Customization',
+                                                ' Salesforce Mobile App Rollout',
+                                                ' App Deployment',
+                                                ' AppExchange App Development',
+                                                ' Quick Start: Build Your First App',
+                                                ' Quick Start: Lightning App Builder',
+                                                ' Build a Battle Station App',
+                                                ' Build a Suggestion Box App',
+                                                ' App Customization Specialist',
+                                            ]
+                                        ]
+                                }
+                            }
+                        }
+                    }
+                }
+            },
+            {
+                $sort: {badges:-1}
+            }
+        ]
+    );
+
     var devOneCert = await trailblazerFactory.aggregate(
         [
             {
@@ -104,6 +353,11 @@ exports.trailblazer_list = async function(req,res){
             }
         ]
     );
+
+
+
+/*
+
     var badgesThisMonth = await trailblazerFactory.aggregate(
         [
             {
@@ -217,6 +471,7 @@ exports.trailblazer_list = async function(req,res){
             }
         ]         
     );
+*/
     newestTrailblazers.forEach((element,index,array) => {
         newestTrailblazers[index].created_at = moment(element.created_at).fromNow();
     });
@@ -259,11 +514,14 @@ exports.trailblazer_list = async function(req,res){
         badgeLeaders: badgeLeaders,
         pointLeaders:pointLeaders,
         newestTrailblazers:newestTrailblazers,
-        badgesThisMonth:badgesThisMonth,
-        badgesLastMonth:badgesLastMonth,
-        badges2MonthsAgo:badges2MonthsAgo,
+       // badgesThisMonth:badgesThisMonth,
+       // badgesLastMonth:badgesLastMonth,
+       // badges2MonthsAgo:badges2MonthsAgo,
         trailblazersByBadges:trailblazersByBadges,
-        devOneCert:devOneCert
+        devOneCert:devOneCert,
+        adminCert:adminCert,
+        advancedAdminCert:advancedAdminCert,
+        platformAppBuilderCert:platformAppBuilderCert,
     }
     console.log(results);
     res.render('pages/index',results);
